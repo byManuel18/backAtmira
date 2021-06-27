dd3c271d59ee0cf4c85ddb1e9a2f9fd487d989f5
Manuel <antoniomanu533@gmail.com>
2021-06-27 19:25:37 +0200

    Formaato de las Fechas y la URL listas

--- src/main/java/com/example/backend/services/AsteroidesService.java
+++ src/main/java/com/example/backend/services/AsteroidesService.java
@@ -7,25 +7,24 @@ import javax.ws.rs.client.Client;
 import org.springframework.stereotype.Service;
 import com.example.backend.models.NearEarthObjectFeed;
 
-
-
 @Service
 public class AsteroidesService {
 	
-	private String url="https://api.nasa.gov/neo/rest/v1/feed?";
-	private String start_date="start_date=";
-	private String end_date="&end_date=";
-	private String apikey="&api_key=zdUP8ElJv1cehFM0rsZVSQN7uBVxlDnu4diHlLSb";
+	private static final String URL="https://api.nasa.gov/neo/rest/v1/feed?";
+	private static final String START_DATE="start_date=";
+	private static final String END_DATE="&end_date=";
+	private static final String APIKEY="&api_key=zdUP8ElJv1cehFM0rsZVSQN7uBVxlDnu4diHlLSb";
+	private static final int PLUSDAYS=7;
+	
 	public void getAlgo() {
 		LocalDate date=LocalDate.now();
+		LocalDate dateend=date.plusDays(this.PLUSDAYS);
 		String datenow=date.toString();
-		date.plusDays(7);
-		String datenext=date.toString();
-		System.out.println(datenow);
-		System.out.println(datenext);
+		String datenext=dateend.toString();
+		String endpoint=this.URL+this.START_DATE+datenow+this.END_DATE+datenext+this.APIKEY;
 		/*Client cliente=ClientBuilder.newClient();
 		
-		NearEarthObjectFeed lista=  cliente.target(url)
+		NearEarthObjectFeed lista=  cliente.target(endpoint)
 		.request(MediaType.APPLICATION_JSON).get(NearEarthObjectFeed.class);
 		cliente.close();
 		System.out.println(lista.getElement_count());*/
